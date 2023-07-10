83acc20696a096c7bdd784e8e1049791747204c2
kmaliaris <kmaliaris@college.harvard.edu>
2023-07-10 17:02:09 -0400

    added plot_lattice

--- lattice.py
+++ lattice.py
@@ -20,9 +20,32 @@ class lattice:
         ax = fig.add_subplot(111, aspect='equal')
 
         #Defining the margin manually to ensure entire plot is visible
-        margin_x = 0.2 * (max(vertices_x) - min(vertices_x))
-        margin_y = 0.2 * (max(vertices_y) - min(vertices_y))
+        margin_x = 0.1 * (max(vertices_x) - min(vertices_x))
+        margin_y = 0.1 * (max(vertices_y) - min(vertices_y))
         ax.set_xlim(min(vertices_x) - margin_x, max(vertices_x) + margin_x)
         ax.set_ylim(min(vertices_y) - margin_y, max(vertices_y) + margin_y)
 
         ax.add_patch(patches.Polygon(xy=list(zip(vertices_x,vertices_y)), fill=False))
+        plt.title('Plot of the [' + str(self.a1) + str(self.a2) + '] unit cell')
+
+    def plot_lattice(self, n, m):
+        """Plots the unit cell of the lattice"""
+        vertices_x = [0, self.a1[0], (self.a1+self.a2)[0], self.a2[0]]
+        vertices_y = [0, self.a1[1], (self.a1+self.a2)[1], self.a2[1]]
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, aspect='equal')
+
+        #Defining the margin manually to ensure entire plot is visible
+        margin = 0.1 * min(n, m) * (max(vertices_x) - min(vertices_x))
+        ax.set_xlim(min(vertices_x) - margin, n * self.a1[0] + m * self.a2[0] + margin)
+        ax.set_ylim(min(vertices_y) - margin, n * self.a1[1] + m * self.a2[1] + margin)
+
+        #Printing the cells one by one in both directions
+        for i in range(0, n):
+            for j in range (0, m):
+                cell_x = vertices_x + i*self.a1[0] + j*self.a2[0]
+                cell_y = vertices_y + i*self.a1[1] + j*self.a2[1]
+                ax.add_patch(patches.Polygon(xy=list(zip(cell_x, cell_y)), fill=False))
+
+        plt.title('Plot of the ' + str(n) + 'x' + str(m) + ' lattice of [' + str(self.a1) + str(self.a2) + ']')
